59508d9ecf8339ef8e546fecd55484ef8a75a4fe
sayf-ismail <63899527+sayf-ismail@users.noreply.github.com>
2022-06-26 00:26:47 +0800

    fix image and layout

--- pages/_app.js
+++ pages/_app.js
@@ -3,34 +3,38 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-  <div className='flex flex-col h-full'>      
-    <header className='flex-none bg-gradient-to-r from-cyan-500 to-fuchsia-500 text-gray-100 font-bold w-full'>
+  <div className='flex flex-col w-full'>      
+    <header className='flex-none bg-gradient-to-r from-slate-200 to-gray-50 text-stone-700 font-bold w-full shadow-md '>
       <nav className='flex'>
-        <ul className="flex-1 text-2xl font-bold py-3 px-12 justify-start">
-          <li>Sayf Ismail</li>
+        <ul className="flex-auto text-2xl font-bold py-3 pl-12 justify-start mb-8">
+          <a href="/"><li className='hover:text-yellow-500'>Sayf Ismail</li></a>
         </ul>
 
-        <ul className='flex flex-1 justify-end gap-12 py-5 px-12 text-grey'>
-          <li><Link href="/">Home</Link></li>
+        <ul className='hidden space-x-8 sm:flex justify-between gap-12 py-5 px-12 text-grey'>
           <li><Link href="/about">About</Link></li>
           <li>Blog</li>
-          <li><a href="/pdf/01_Resume_Sayf_Ismail.pdf" target="_blank">Resume</a></li>
+          <li><a href="https://drive.google.com/file/d/1sYphYht7gNwCBBYx4LW9E8d8D-PcXcH_/view?usp=sharing" target="_blank">Resume</a></li>
         </ul>
+        <div class="sm:hidden space-y-2 m-auto pr-12">
+          <span class="block w-8 h-1 bg-gray-700"></span>
+          <span class="block w-8 h-1 bg-gray-700"></span>
+          <span class="block w-8 h-1 bg-gray-700"></span>
+        </div>
       </nav>
     </header>
-    <div className='flex'>
+    <div className='my-8 justify-center items-center hover:shadow-lg'>
      <Component {...pageProps}/>
     </div>
-    <footer className='flex-none w-full bg-gradient-to-r from-fuchsia-500 to-cyan-500 py-8 text-gray-100 font-bold'>
-      <ul className='flex flex-1 justify-center gap-20'>
-          <li>Linkedin</li>
-          <li>Github</li>
-          <li>Twitter</li>
+    <footer className='bottom-0 flex-none w-full bg-gradient-to-r from-slate-200 to-gray-50 py-8 text-stone-700 font-bold'>
+      <ul className='flex space-y-2 flex-col md:flex-row justify-center gap-1 md:gap-20 items-center'>
+          <li><a href="https://github.com/sayf-ismail" target="_blank">Github</a></li>
+          <li><a href="https://www.linkedin.com/in/sayf-ismail/" target="_blank">Linkedin</a></li>
+          <li><a href="https://twitter.com/Sayfs0" target="_blank">Twitter</a></li>
           <li>Blog</li>
-          <li>Contact Me</li>
+          <li><a href="mailto:sayfti@gmail.com">Contact Me</a></li>
       </ul>
     </footer>
-    <section className='flex-none w-full bg-gradient-to-r from-fuchsia-500 to-cyan-500 py-8 text-gray-100 mt-0 mx-auto text-center'>
+    <section className='flex-none w-full bg-gradient-to-r from-slate-200 to-gray-50 py-8 text-stone-700 mt-0 mx-auto text-center'>
     © 2022 Sayf Ismail. All rights reserved.
     </section>
   </div>
--- pages/index.js
+++ pages/index.js
@@ -2,45 +2,89 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className='grow h-full items-center sm:bg-yellow-300 md:bg-green-300 lg:bg-pink-300 xl:bg-blue-300 2xl:bg-red-300'>
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mx-auto justify-center gap-1 max-w-7xl'>
-        <Image
-          src="/GoodTomato.png"         alt="Picture of the author"
-          width={600}
-          height={600} 
-          className="sm:mt-10"
-        />
-        <Image 
-          src="/GoodTomato.png"         alt="Picture of the author"
-          width={600}
-          height={600} 
-          className="sm:mt-10"
-        />
-        <Image 
-          src="/GoodTomato.png"         alt="Picture of the author"
-          width={600}
-          height={600} 
-          className="sm:mt-10"
-        />
-        <Image 
-          src="/GoodTomato.png"         alt="Picture of the author"
-          width={600}
-          height={600} 
-          className="sm:mt-10"
-        />
-        <Image 
-          src="/GoodTomato.png"         alt="Picture of the author"
-          width={600}
-          height={600} 
-          className="sm:mt-10"
-        />
-        <Image 
-          src="/GoodTomato.png"         alt="Picture of the author"
-          width={600}
-          height={600} 
-          className="sm:mt-10"
-        />
-      </div>
+    // <main className='items-center sm:bg-yellow-300 md:bg-green-300 lg:bg-pink-300 xl:bg-blue-300 2xl:bg-red-300 w-full'>
+    <main className=' items-center'>
+      <div className='grid grid-cols-1 auto-cols-auto sm:grid-cols-2 md:grid-cols-3 justify-center gap-0 items-center p-0 m-0'>
+        
+          <div className='bg-gray-500'>
+            <div className='btn items-center rounded-md shadow-lg mix-blend-overlay'>
+              <a href="/" className=''>
+                <Image 
+                  src="/static/dome.jpg"         alt="Booterfly!"
+                  width={"800"}
+                  height={"800"} 
+                  objectFit="cover"          
+                  className='rounded-xl'
+                />
+              </a>
+            </div>
+          </div>
+          <div className='bg-gray-500'>
+            <div className='btn items-center rounded-md shadow-lg mix-blend-overlay'>
+              <a href="/" className=''>
+                <Image 
+                  src="/static/caterpillar.jpeg"         alt="Booterfly!"
+                  width={"800"}
+                  height={"800"} 
+                  objectFit="cover"          
+                  className='rounded-xl'
+                />
+              </a>
+            </div>
+          </div>
+          <div className='bg-gray-500'>
+            <div className='btn items-center rounded-md shadow-lg mix-blend-overlay'>
+              <a href="/" className=''>
+                <Image 
+                  src="/static/butterfly_park.jpeg"         alt="Booterfly!"
+                  width={"800"}
+                  height={"800"} 
+                  objectFit="cover"          
+                  className='rounded-xl'
+                />
+              </a>
+            </div>
+          </div>
+          <div className='bg-gray-500'>
+            <div className='btn items-center rounded-md shadow-lg mix-blend-overlay'>
+              <a href="mailto:sayfti@gmail.com" className=''>
+                <Image 
+                  src="/static/hill.jpeg"         alt="Booterfly!"
+                  width={"800"}
+                  height={"800"} 
+                  objectFit="cover"          
+                  className='rounded-xl'
+                />
+              </a>
+            </div>
+          </div>
+          <div className='bg-gray-500'>
+            <div className='btn items-center rounded-md shadow-lg mix-blend-overlay'>
+              <a href="/" className=''>
+                <Image 
+                  src="/static/shop_fish.jpeg"         alt="Booterfly!"
+                  width={"800"}
+                  height={"800"} 
+                  objectFit="cover"          
+                  className='rounded-xl'
+                />
+              </a>
+            </div>
+          </div>
+          <div className='bg-gray-500'>
+            <div className='btn items-center rounded-md shadow-lg mix-blend-overlay'>
+              <a href="mailto:sayfti@gmail.com" className=''>
+                <Image 
+                  src="/static/btfly.jpg"         alt="Booterfly!"
+                  width={"800"}
+                  height={"800"} 
+                  objectFit="cover"          
+                  className='rounded-xl'
+                />
+              </a>
+            </div>
+          </div>
+        </div>
     </main>
   )
 }
