9ba4dbb73f717367caef71482d714aeeae8d4e25
sayf-ismail <63899527+sayf-ismail@users.noreply.github.com>
2022-06-27 11:27:26 +0800

    initial terminal

--- pages/_app.js
+++ pages/_app.js
@@ -1,43 +1,46 @@
 import Link from 'next/link'
+import { TerminalContextProvider } from 'react-terminal'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-  <div className='flex flex-col w-full'>      
-    <header className='flex-none bg-gradient-to-r from-slate-200 to-gray-50 text-stone-700 font-bold w-full shadow-md '>
-      <nav className='flex'>
-        <ul className="flex-auto text-2xl font-bold py-3 pl-12 justify-start mb-8">
-          <a href=""><li className='hover:text-yellow-500'>Sayf Ismail</li></a>
-        </ul>
+    <TerminalContextProvider>
+      <div className='flex flex-col w-full'>      
+        <header className='flex-none bg-gradient-to-r from-slate-200 to-gray-50 text-stone-700 font-bold w-full shadow-md '>
+          <nav className='flex'>
+            <ul className="flex-auto text-2xl font-bold py-3 pl-12 justify-start mb-8">
+              <a href=""><li className='hover:text-yellow-500'>Sayf Ismail</li></a>
+            </ul>
 
-        <ul className='hidden space-x-8 sm:flex justify-between gap-12 py-5 px-12 text-grey'>
-          <li><Link href="/about">About</Link></li>
-          <li>Blog</li>
-          <li><a href="https://drive.google.com/file/d/1sYphYht7gNwCBBYx4LW9E8d8D-PcXcH_/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a></li>
-        </ul>
-        <div className="sm:hidden space-y-2 m-auto pr-12">
-          <span className="block w-8 h-1 bg-gray-700"></span>
-          <span className="block w-8 h-1 bg-gray-700"></span>
-          <span className="block w-8 h-1 bg-gray-700"></span>
+            <ul className='hidden space-x-8 sm:flex justify-between gap-12 py-5 px-12 text-grey'>
+              <li><Link href="/about">About</Link></li>
+              <li>Blog</li>
+              <li><a href="https://drive.google.com/file/d/1sYphYht7gNwCBBYx4LW9E8d8D-PcXcH_/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a></li>
+            </ul>
+            <div className="sm:hidden space-y-2 m-auto pr-12">
+              <span className="block w-8 h-1 bg-gray-700"></span>
+              <span className="block w-8 h-1 bg-gray-700"></span>
+              <span className="block w-8 h-1 bg-gray-700"></span>
+            </div>
+          </nav>
+        </header>
+        <div className='my-8 justify-center items-center'>
+        <Component {...pageProps}/>
         </div>
-      </nav>
-    </header>
-    <div className='my-8 justify-center items-center'>
-     <Component {...pageProps}/>
-    </div>
-    <footer className='bottom-0 flex-none w-full bg-gradient-to-r from-slate-200 to-gray-50 py-8 text-stone-700 font-bold'>
-      <ul className='flex space-y-2 flex-col md:flex-row justify-center gap-1 md:gap-20 items-center'>
-          <li><a href="https://github.com/sayf-ismail" target="_blank" rel="noreferrer">Github</a></li>
-          <li><a href="https://www.linkedin.com/in/sayf-ismail/" target="_blank" rel="noreferrer">Linkedin</a></li>
-          <li><a href="https://twitter.com/Sayfs0" target="_blank" rel="noreferrer">Twitter</a></li>
-          <li>Blog</li>
-          <li><a href="mailto:sayfti@gmail.com">Contact Me</a></li>
-      </ul>
-    </footer>
-    <section className='flex-none w-full bg-gradient-to-r from-slate-200 to-gray-50 py-8 text-stone-700 mt-0 mx-auto text-center'>
-    © 2022 Sayf Ismail. All rights reserved. Built with Next.js, Tailwind and Vercel.
-    </section>
-  </div>
+        <footer className='bottom-0 flex-none w-full bg-gradient-to-r from-slate-200 to-gray-50 py-8 text-stone-700 font-bold'>
+          <ul className='flex space-y-2 flex-col md:flex-row justify-center gap-1 md:gap-20 items-center'>
+              <li><a href="https://github.com/sayf-ismail" target="_blank" rel="noreferrer">Github</a></li>
+              <li><a href="https://www.linkedin.com/in/sayf-ismail/" target="_blank" rel="noreferrer">Linkedin</a></li>
+              <li><a href="https://twitter.com/Sayfs0" target="_blank" rel="noreferrer">Twitter</a></li>
+              <li>Blog</li>
+              <li><a href="mailto:sayfti@gmail.com">Contact Me</a></li>
+          </ul>
+        </footer>
+        <section className='flex-none w-full bg-gradient-to-r from-slate-200 to-gray-50 py-8 text-stone-700 mt-0 mx-auto text-center'>
+        © 2022 Sayf Ismail. All rights reserved. Built with Next.js, Tailwind and Vercel.
+        </section>
+      </div>
+    </TerminalContextProvider>
   )
 }
 
--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,56 @@
 import Link from 'next/link';
+import { ReactTerminal } from 'react-terminal';
+
 
 const About = () => {
+
+  // let keywordsList = [
+  //   "`WHOIS` - about me...",
+  //   "[`SOCIALS` - lets connect!...]",
+  //   "[`PROJECTS` - come explore...]",
+  //   "[`WHOIS` - about me :)...]",
+  //   "[`WHOIS` - about me :)...]",
+  // ];
+
+  const commands = {
+    help: (
+      <span>
+        <strong>clear</strong> - clears the console. <br/>
+        <strong>socials</strong> - links to connect. <br/>
+        <strong>projects</strong> - list of projects. <br/>
+        <strong>secret</strong> - don't type this! <br/>
+      </span>
+      ),
+    whois: "Call me Sayf!",
+    cd: (directory) => `changed path to ${directory}`,
+    banner: (
+      <> 
+        <p>  __   __       ___   ___      ___   ___ </p> 
+        <p> (  \,/  )     ( @ \Y/ @ )    ((o)\,/(o))</p>
+        <p>  \_ | _/       \__+|+__/      \__ | __/</p>
+        <p>  "(_/ \_)        {_/ \_}        {_/ \_} "</p>
+      </>
+      ),
+    this: "I said don't! "
+  }
+
   return (
-    <div>
-      ABOUT PAGE
-      <Link href="">Back to Home</Link>
-    </div>
+    <>
+      <h1>ABOUT PAGE</h1>
+      <div className='bg-gray-300 w-3/4 mx-auto'>
+
+        <p>Type 'help' to get started ;)</p>
+        
+        <div>
+          <ReactTerminal 
+            theme="dracula"
+            commands={commands} 
+          />
+        </div>
+
+        <Link href="/">Back to Home</Link>
+      </div>
+    </>
   )
 };
 
